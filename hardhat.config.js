d676cd98e5014dbd13dc642df519b0c39bce8446
Pratyan Majumder <heypratyan@gmail.com>
2022-12-27 02:14:54 +0530

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,6 @@
 require("@nomicfoundation/hardhat-toolbox");
 
-const ALCHEMY_API_KEY = "KZUaQ0B9KCzGCeKzOeqHtSemk1LNzJioM";
+const ALCHEMY_API_KEY = "";
 const GOERLI_PRIVATE_KEY = "";
 
 /** @type import('hardhat/config').HardhatUserConfig */
@@ -8,7 +8,7 @@ module.exports = {
   solidity: "0.8.17",
   networks: {
     goerli: {
-      url: `https://eth-goerli.g.alchemy.com/v2/ZUaQ0B9KCzGCeKzOeqHtSemk1LNzJioM`,
+      url: `https://eth-goerli.g.alchemy.com/v2/`,
       accounts: [GOERLI_PRIVATE_KEY]
     }
   }
